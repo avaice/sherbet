e4bd307f43cf39e757c576d089fce4690143b7cb
avaice <dev@cho-ice.xyz>
2025-02-27 01:55:07 +0900

    load settings from .env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 mod handler;
 
+use std::env;
 use crate::handler::handle_client;
 use std::net::TcpListener;
+use dotenvy::dotenv;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Listening on http://127.0.0.1:7878");
+
+    // Load environment variables from .env file
+    dotenv().ok(); 
+
+    let http_port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", http_port)).unwrap();
+    println!("Listening on port {}", http_port);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
