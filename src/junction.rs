7aedb22c8c340def0c3f5f8ded948d9a49f37460
avaice <dev@cho-ice.xyz>
2025-03-14 12:21:21 +0900

    add directory handling and redirect to index.html

--- src/junction.rs
+++ src/junction.rs
@@ -1,10 +1,14 @@
-use std::{env, fs, net::TcpStream, path::Path};
+use crate::{
+    factory::plain_text_factory,
+    pong::{send_binary_response, send_response},
+};
 use mime_guess::from_path;
-use crate::{factory::plain_text_factory, pong::{send_binary_response, send_response}};
+use std::{env, fs, net::TcpStream, path::Path};
 
 pub fn router(uri: &str, stream: TcpStream) {
     let static_dir = env::var("STATIC_DIR").unwrap_or("public".to_string());
-    let static_dir = Path::new(&static_dir).canonicalize()
+    let static_dir = Path::new(&static_dir)
+        .canonicalize()
         .expect("Failed to canonicalize static directory");
 
     let file_path = if uri == "/" {
@@ -15,6 +19,16 @@ pub fn router(uri: &str, stream: TcpStream) {
 
     if let Ok(canon_path) = file_path.canonicalize() {
         if canon_path.starts_with(&static_dir) {
+            if canon_path.is_dir() {
+                let index_path = canon_path.join("index.html");
+                let new_uri = format!("{}{}", uri, "index.html");
+                println!("Redirecting to {}", new_uri);
+                if index_path.exists() {
+                    router(&new_uri, stream);
+                    return;
+                }
+            }
+
             if let Ok(contents) = fs::read(&canon_path) {
                 let mime_type = from_path(&canon_path).first_or_octet_stream();
                 send_binary_response(200, stream, &contents, &mime_type);
